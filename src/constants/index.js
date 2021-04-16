3f18889bd79b965838937ff4e2e74dddca079450
LucaSilverhand <levinswap0@gmail.com>
2021-04-16 21:14:56 +0400

    tokenlists

--- src/constants/index.js
+++ src/constants/index.js
@@ -11,12 +11,12 @@ export const timeframeOptions = {
 }
 
 // token lists
-const LEVINSWAP_LIST = 'https://ipfs.io/ipfs/QmVjmLAgkWuNxHAE9Um9KuwEJmTEk82LzdEqLnfiZ5NQpR?filename=levinswap-default.tokenlist.json'
+const LEVINSWAP_LIST = 'https://ipfs.io/ipfs/QmUWxthidUYXUJ2kiZLLPxkMKYDAinnpA591R3SRN6wufs?filename=levinswap-default.tokenlist.json'
 const LEVINSWAP_STABLECOIN_LIST = 'https://ipfs.io/ipfs/QmWrhnRTCQ8CgSoNmHV6WsneLLhErouD4fQPpSaqhsibpD?filename=levinswap-stablecoin-tokenlist.json'
 const REALT_LIST = 'https://api.realt.community/v1/tokenList'
-const REN_LIST = 'https://ipfs.io/ipfs/QmS55aGgkVoZLj87qP2VZMrnmv5PupMsDVvUp6tBHfReLF?filename=ren-default.tokenlist.json'
-const SYNTHETIX_LST = 'https://ipfs.io/ipfs/QmbZvrV7YsF6eKP8umoauqi7FFTG4KgtQBjTeLfVFJpxZj?filename=synthetix-default.tokenlist.json'
-const BSC_LIST = 'https://ipfs.io/ipfs/QmbFFNszCPSUZktXYdjVd7wSaqnMxeXedrQWC5zcuEk8pv?filename=bsc-default.tokenlist.json'
+const REN_LIST = 'https://ipfs.io/ipfs/QmSqYWWdzfngm57int8AesJr1APRBBYxC8knmYC9rN4ECj?filename=ren-default.tokenlist.json'
+const SYNTHETIX_LST = 'https://ipfs.io/ipfs/QmYeWjmKW4tQ6JfCG9XUkVa9CFBAUY36Z1W9w1iM6Bx4ne?filename=synthetix-default.tokenlist.json'
+const BSC_LIST = 'https://ipfs.io/ipfs/QmcT2RnZ2tJcpfxaruy1EkKAF6aGCRrVsNFfn5oSCQLPQJ?filename=bsc-default.tokenlist.json'
 
 // token list urls to fetch tokens from - use for warnings on tokens and pairs
 export const SUPPORTED_LIST_URLS__NO_ENS = [LEVINSWAP_LIST, LEVINSWAP_STABLECOIN_LIST, REALT_LIST, REN_LIST, SYNTHETIX_LST, BSC_LIST]
